4cf184cc4f4c6e6c98389afb1858b7102bcbbd0a
Alex-GHP <alex.morariu.dev@gmail.com>
2025-12-23 18:35:00 +0200

    feat: agents

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import argparse
 import os
+import sys
 
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from prompts import system_prompt
+
 from call_function import available_functions, call_function
+from config import MAX_ITERS
+from prompts import system_prompt
 
 
 def main():
@@ -24,14 +27,30 @@ def main():
     if args.verbose:
         print(f"User prompt: {args.user_prompt}\n")
 
-    generate_content(client, messages, args.verbose)
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, args.verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
 
 
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
@@ -40,11 +59,13 @@ def generate_content(client, messages, verbose):
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if not response.function_calls:    
-        print("Response:")
-        print(response.text)
-        return
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
 
+    if not response.function_calls:
+        return response.text
 
     function_responses = []
     for function_call in response.function_calls:
@@ -58,6 +79,8 @@ def generate_content(client, messages, verbose):
         if verbose:
             print(f"-> {result.parts[0].function_response.response}")
         function_responses.append(result.parts[0])
+
+    messages.append(types.Content(role="user", parts=function_responses))
 
 
 if __name__ == "__main__":
